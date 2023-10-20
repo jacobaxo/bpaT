4983d62686d02c3fcc58356bbe29ac4da5f391a5
mays_jacobj <mays_jacobj@G-02-19>
2023-10-20 11:37:49 -0400

    Add project files.

---
+++ bpaT/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfQuizApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            int score = 0;
+
+            if (Q1Option1.IsChecked == true)
+            {
+                score++;
+            }
+            if (Q2Option3.IsChecked == true)
+            {
+                score++;
+            }
+            if (Q3Option1.IsChecked == true)
+            {
+                score++;
+            }
+
+            MessageBox.Show("Your score: " + score + "/3");
+        }
+    }
+}
